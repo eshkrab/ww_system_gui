9bbcb66a5cc97197a333fa7b78924fe79728af60
rina shkrabova <eshkrab@gmail.com>
2023-07-04 13:16:43 -0700

    trying to set server ip via not hardcoded lmao

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,5 +4,9 @@ set -euo pipefail
 # Generate nginx.conf with substituted environment variables
 envsubst '${server_ip} ${server_port}' < /etc/nginx/conf.d/nginx.template > /etc/nginx/conf.d/default.conf
 
+# Generate index.html from template with substituted environment variables
+envsubst '${server_ip} ${server_port}' < /usr/share/nginx/html/index.html.template > /usr/share/nginx/html/index.html
+
+
 # Start nginx in foreground
 nginx -g 'daemon off;'
